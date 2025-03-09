3fa428ba850ca0d5ae2b6897d30dae68560f8504
Michele Bertoncini <158177022+MBertoncini@users.noreply.github.com>
2025-03-09 14:31:28 +0100

    minor fixes

--- apiario_manager/settings.py
+++ apiario_manager/settings.py
@@ -12,9 +12,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yourkey12345678901234567890'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Cible99.pythonanywhere.com', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
