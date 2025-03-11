0aa36b2a1c6270b37146592a647be623597f70a9
Michele Bertoncini <158177022+MBertoncini@users.noreply.github.com>
2025-03-12 00:03:55 +0100

    to pythonAnywhere

--- apiario_manager/settings.py
+++ apiario_manager/settings.py
@@ -12,9 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-yourkey12345678901234567890')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['Cible99.pythonanywhere.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['Cible99.pythonanywhere.com']
+#ALLOWED_HOSTS = ['Cible99.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
